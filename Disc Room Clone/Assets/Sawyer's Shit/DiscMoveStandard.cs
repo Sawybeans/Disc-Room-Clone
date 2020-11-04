a4527084a876012e9a14fd461a0934545880ffff
Sawyer Smith <sps471@nyu.edu>
2020-11-04 12:44:13 -0500

    Class 1-4-2020
    Finished the wall roaming discs and bigger, slower discs

--- Disc Room Clone/Assets/Sawyer's Shit/DiscMoveStandard.cs
+++ Disc Room Clone/Assets/Sawyer's Shit/DiscMoveStandard.cs
@@ -53,14 +53,13 @@ public class DiscMoveStandard : MonoBehaviour
 
         if (Physics2D.Raycast(rightRay.origin, rightRay.direction, rayDist))
         {
-            if (bounceTimer == 0)
+            
+            if (hitRight.collider != null)
             {
-                if (hitRight.collider != null)
-                {
-                    discSpeedX *= -1;
-                    bounceTimer = 10f;
-                }
+                discSpeedX *= -1;
+                bounceTimer = 10f;
             }
+            
         }
 
         Ray leftRay = new Ray(transform.position, Vector2.left);
@@ -70,14 +69,13 @@ public class DiscMoveStandard : MonoBehaviour
         if (Physics2D.Raycast(leftRay.origin, leftRay.direction, rayDist))
         {
 
-            if (bounceTimer == 0)
+            
+            if (hitLeft.collider != null)
             {
-                if (hitLeft.collider != null)
-                {
-                    discSpeedX *= -1;
-                    bounceTimer = 10f;
-                }
+                discSpeedX *= -1;
+                bounceTimer = 10f;
             }
+            
         }
 
         Ray topRay = new Ray(transform.position, Vector2.up);
@@ -87,14 +85,14 @@ public class DiscMoveStandard : MonoBehaviour
         if (Physics2D.Raycast(topRay.origin, topRay.direction, rayDist))
         {
 
-            if (bounceTimer == 0) { 
-                if (hitTop.collider != null)
-                {
-                    discSpeedY *= -1;
-                    bounceTimer = 10f;
-                }
-
+            
+            if (hitTop.collider != null)
+            {
+                discSpeedY *= -1;
+                bounceTimer = 10f;
             }
+
+            
         }
 
         Ray bottomRay = new Ray(transform.position, Vector2.down);
@@ -103,16 +101,15 @@ public class DiscMoveStandard : MonoBehaviour
 
         if (Physics2D.Raycast(bottomRay.origin, bottomRay.direction, rayDist))
         {
-            if (bounceTimer == 0) { 
-                if (hitBottom.collider != null)
-                {
-                    discSpeedY *= -1;
-                    bounceTimer = 10f;
-                }
+            
+            if (hitBottom.collider != null)
+            {
+                discSpeedY *= -1;
+                bounceTimer = 10f;
             }
+            
         }
 
-        else { bounceTimer--; }
 
         //Always be moving on the X and Y axis
         thisRigidbody2d.velocity = new Vector3(discSpeedX, discSpeedY);
