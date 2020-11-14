5a1fad91d0f47cc1fcd86309ff76409886103913
Sawyer Smith <sps471@nyu.edu>
2020-11-14 14:37:49 -0500

    Disc Spawner + Prefabs

--- Disc Room Clone/Assets/Sawyer's Shit/DiscSpawn.cs
+++ Disc Room Clone/Assets/Sawyer's Shit/DiscSpawn.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class DiscSpawn : MonoBehaviour
 {
+
+    public GameObject discStandard;
+    public GameObject discBig;
+    public GameObject discWall;
+    public GameObject discTarget;
+
+    public float spawn;
+    public float spawnTimer = 800f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawn = spawnTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (spawn > 0)
+        {
+            spawn--;
+        }
+
+        if (spawn == 0)
+        {
+            int newDisc = Random.Range(0, 5);
+
+            if (newDisc == 0)
+            {
+                Instantiate(discStandard, new Vector3(0, 0, 0), Quaternion.identity);
+            }
+
+            if (newDisc >= 1)
+            {
+                Instantiate(discBig, new Vector3(0, 0, 0), Quaternion.identity);
+            }
+
+            spawn = spawnTimer;
+        }
     }
 }
