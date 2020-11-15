20b7c72695b95362a055086170650eb7e071c6dd
Yuhao Gao <yg1766@nyu.edu>
2020-11-16 01:17:28 +0800

    Blood particle/spawn player

---
+++ Disc Room Clone/Assets/Harry's SHIT/BloodControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject bloodSelf;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(bloodSelf, 1f);
+    }
+}
