1ca82561e48a75d4d29f93224d7adeb5789752d5
Yifan <ym2159@nyu.edu>
2020-12-01 21:16:20 -0800

    additional works

---
+++ Disc Room Clone/Assets/Yifan Folder/scripts/buttonManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonManager : MonoBehaviour
+{
+    public Button room1, room2, room3, room4, room5, room6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        room4.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Disc Room Clone/Assets/Yifan Folder/scripts/splashscreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class splashscreen : MonoBehaviour
+{
+    public void switchToSplashScreen(string sceneName)
+    {
+
+        Application.LoadLevel(sceneName);
+    }
+}
